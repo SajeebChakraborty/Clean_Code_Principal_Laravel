9c7380d63e753dc4ab8385e3535750dd1d5f8c02
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-25 04:27:39 +0600

    use componenet

--- resources/views/components/form/textbox.blade.php
+++ resources/views/components/form/textbox.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
-    <label for="exampleInputEmail1">Name</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <label for="exampleInputEmail1">{{ $title }}</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="{{ $placeholder }}">
 </div>

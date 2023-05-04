1cd280a11b31ee3546903152ee80e308dbaa86f3
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-05-04 22:22:37 +0600

    bug fixed

--- resources/views/components/form/textbox.blade.php
+++ resources/views/components/form/textbox.blade.php
@@ -1,5 +1,6 @@
 <div class="form-group">
     <label for="exampleInputEmail1">{{ $title }}</label>
+    
     <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="{{ $placeholder }}">
      
 </div>

7f68188ab6db52160eb6411e771ade9a889b0c11
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-05-04 22:11:25 +0600

    update

--- resources/views/components/form/textbox.blade.php
+++ resources/views/components/form/textbox.blade.php
@@ -1,5 +1,4 @@
 <div class="form-group">
     <label for="exampleInputEmail1">{{ $title }}</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="{{ $placeholder }}">
-    
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="{{ $placeholder }}"> 
 </div>
